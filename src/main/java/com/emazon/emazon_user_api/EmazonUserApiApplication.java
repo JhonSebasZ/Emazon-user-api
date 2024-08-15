09d7d54067ab7629b394180f8f28de93913a5881
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2024-08-15 09:59:45 -0500

    first commit

---
+++ src/main/java/com/emazon/emazon_user_api/EmazonUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.emazon_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/emazon_user_api/EmazonUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.emazon_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
